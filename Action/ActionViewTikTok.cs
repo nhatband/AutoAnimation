b089096bab27b26d984994154ff0243f04da48a0
nhatbanda <nhat.nguyen@icomm.vn>
2022-04-01 23:52:37 +0700

    commit

--- Action/ActionViewTikTok.cs
+++ Action/ActionViewTikTok.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Icomm.ForumClient;
+using Microsoft.Extensions.Configuration;
+using OpenQA.Selenium;
+using Serilog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,30 @@ namespace TikTokAutoAnimation
 {
     class ActionViewTikTok
     {
+        private readonly IConfiguration configuration;
+        private IWebDriver driver;
 
+        public ActionViewTikTok(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public int Execute()
+        {
+            Log.Information("Running");
+            var RootDir = configuration.GetSection("RootDir").Value;
+            var util = new MyChromeDrive();
+            driver = util.InitChromeAsync("", 0, RootDir);
+            driver.Navigate().GoToUrl("https://www.tiktok.com/vi-VN/");
+            var resLogin = Login();
+            if (resLogin != 2)
+                return -1;
+
+            return 0;
+        }
+        public int Login()
+        {
+            return -1;
+        }
     }
 }
